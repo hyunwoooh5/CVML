dd2a1e87dab5029484b164ed5dd47aeea9d6bbf3
Hyunwoo Oh <hyunwooh@umd.edu>
2023-08-24 14:40:10 -0400

    add cv.py and observable

---
+++ cv.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+
+
+from models import scalar
+from mc import metropolis, replica
+import pickle
+import sys
+import time
+from typing import Callable, Sequence
+
+import flax
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+# Don't print annoying CPU warning.
+jax.config.update('jax_platform_name', 'cpu')
+jax.config.update("jax_debug_nans", True)
+jax.config.update("jax_debug_infs", True)
+
+
+class MLP(nn.Module):
+    features: Sequence[int]
+    kernel_init: Callable = nn.initializers.variance_scaling(
+        1e-5, "fan_in", "truncated_normal")
+    bias_init: Callable = nn.initializers.zeros
+
+    @nn.compact
+    def __call__(self, x):
+        for feat in self.features:
+            x = nn.Dense(feat, kernel_init=self.kernel_init,
+                         bias_init=self.bias_init)(x)
+            x = nn.relu(x)
+        x = nn.Dense(1, kernel_init=self.kernel_init,
+                     bias_init=self.bias_init)(x)
+        return x
+
+
+class Naive(nn.Module):
+    def __call__(self, x):
+        return x
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Train g",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        fromfile_prefix_chars='@')
+    parser.add_argument('model', type=str, help="model filename")
+    parser.add_argument('cv', type=str, help="cv filename")
+    parser.add_argument('-R', '--real', action='store_true',
+                        help="output the real plane")
+    parser.add_argument('-i', '--init', action='store_true',
+                        help="re-initialize even if contour already exists")
+    parser.add_argument('-f', '--from', dest='fromfile', type=str,
+                        help="initialize from other file")
+    parser.add_argument('-l', '--layers', type=int, default=0,
+                        help='number of (hidden) layers')
+    parser.add_argument('-w', '--width', type=int, default=1,
+                        help='width (scaling)')
+    parser.add_argument('-r', '--replica', action='store_true',
+                        help="use replica exchange")
+    parser.add_argument('-nrep', '--nreplicas', type=int, default=30,
+                        help="number of replicas (with -r)")
+    parser.add_argument('-maxh', '--max-hbar', type=float, default=10.,
+                        help="maximum hbar (with -r)")
+    parser.add_argument('--seed', type=int, default=0, help="random seed")
+    parser.add_argument('--seed-time', action='store_true',
+                        help="seed PRNG with current time")
+    parser.add_argument('-lr', '--learningrate', type=float, default=1e-4,
+                        help="learning rate")
+    parser.add_argument('-S', '--skip', default=30, type=int,
+                        help='number of steps to skip')
+    parser.add_argument('-N', '--nstochastic', default=1, type=int,
+                        help="number of samples to estimate gradient")
+    parser.add_argument('-T', '--thermalize', default=0, type=int,
+                        help="number of MC steps (* d.o.f) to thermalize")
+    parser.add_argument('-Nt', '--tsteps', default=10000000, type=int,
+                        help="number of training")
+    parser.add_argument('-o',  '--optimizer', choices=['adam', 'sgd', 'yogi'], default='adam',
+                        help='optimizer to use')
+    parser.add_argument('-s', '--schedule', action='store_true',
+                        help="scheduled learning rate")
+    parser.add_argument('-C', '--care', type=float, default=1,
+                        help='scaling for learning schedule')
+    parser.add_argument('--b1', type=float, default=0.9,
+                        help="b1 parameter for adam")
+    parser.add_argument('--b2', type=float, default=0.999,
+                        help="b2 parameter for adam")
+    parser.add_argument('--weight', type=str, default='jnp.ones(len(grads))',
+                        help="weight for gradients")
+
+    args = parser.parse_args()
+
+    seed = args.seed
+    if args.seed_time:
+        seed = time.time_ns()
+    key = jax.random.PRNGKey(seed)
+
+    with open(args.model, 'rb') as f:
+        model = eval(f.read())
+    V = model.dof
+
+    skip = args.skip
+    if args.skip == 30:
+        skip = V
+
+    g_ikey, chain_key = jax.random.split(key, 2)
+
+    if args.real:
+        # Output real plane and quit
+        g = Naive()
+        g_params = g.init(g_ikey, jnp.zeros(V))
+        with open(args.cv, 'wb') as f:
+            pickle.dump((g, g_params), f)
+        sys.exit(0)
+
+    loaded = False
+    if not args.init and not args.fromfile:
+        try:
+            with open(args.cv, 'rb') as f:
+                g, g_params = pickle.load(f)
+            loaded = True
+        except FileNotFoundError:
+            pass
+    if args.fromfile:
+        with open(args.fromfile, 'rb') as f:
+            g, g_params = pickle.load(f)
+        loaded = True
+    if not loaded:
+        g = MLP([args.width*V]*args.layers)
+        g_params = g.init(g_ikey, jnp.zeros(V))
+
+    # setup metropolis
+    if args.replica:
+        chain = replica.ReplicaExchange(model.action, jnp.zeros(
+            V), chain_key, delta=1./jnp.sqrt(V), max_hbar=args.max_hbar, Nreplicas=args.nreplicas)
+    else:
+        chain = metropolis.Chain(model.action, jnp.zeros(
+            V), chain_key, delta=1./jnp.sqrt(V))
+
+    # define subtraction function
+    f = jax.jit(jax.grad(lambda x, p: g.apply(p, x),
+                argnum=1) - jax.grad(model.action) * g)
+
+    # define loss function
+    @jax.jit
+    def Loss(x, p):
+        o = model.observe(x)
+        return - 2. * model.observe(x) * f(x, p) + f(x, p)**2
+
+    Loss_grad = jax.jit(jax.grad(lambda x, p: Loss(x, p), argnums=1))
+
+    if args.schedule:
+        sched = optax.exponential_decay(
+            init_value=args.learningrate,
+            transition_steps=int(args.care*1000),
+            decay_rate=0.99,
+            end_value=2e-5)
+    else:
+        sched = optax.constant_schedule(args.learningrate)
+    opt = getattr(optax, args.optimizer)(sched, args.b1, args.b2)
+    opt_state = opt.init(g_params)
+    opt_update_jit = jax.jit(opt.update)
+
+    def save():
+        with open(args.cv, 'wb') as f:
+            pickle.dump((g, g_params), f)
+
+    def Grad_Mean(grads, weight):
+        """
+        Params:
+            grads: Gradients
+            weight: Weights
+        """
+        grads_w = [jax.tree_util.tree_map(
+            lambda x: w*x, g) for w, g in zip(weight, grads)]
+        w_mean = jnp.mean(weight)
+        grad_mean = jax.tree_util.tree_map(
+            lambda *x: jnp.mean(jnp.array(x), axis=0)/w_mean, *grads_w)
+        return grad_mean
+
+    def bootstrap(xs, ws=None, N=100, Bs=50):
+        if Bs > len(xs):
+            Bs = len(xs)
+        B = len(xs)//Bs
+        if ws is None:
+            ws = xs*0 + 1
+        # Block
+        x, w = [], []
+        for i in range(Bs):
+            x.append(sum(xs[i*B:i*B+B]*ws[i*B:i*B+B])/sum(ws[i*B:i*B+B]))
+            w.append(sum(ws[i*B:i*B+B]))
+        x = np.array(x)
+        w = np.array(w)
+        # Regular bootstrap
+        y = x * w
+        m = (sum(y) / sum(w))
+        ms = []
+        for _ in range(N):
+            s = np.random.choice(range(len(x)), len(x))
+            ms.append((sum(y[s]) / sum(w[s])))
+        ms = np.array(ms)
+        return m, np.std(ms.real) + 1j*np.std(ms.imag)
+
+    steps = int(10000 / args.nstochastic)
+    grads = [0] * args.nstochastic
+    weight = eval(args.weight)
+
+    # measurement
+    obs = [0] * 10000
+    cvs = [0] * 10000
+
+    chain.calibrate()
+    chain.step(N=args.thermalize*V)
+    try:
+        for t in range(args.tsteps):
+            for s in range(steps):
+                chain.calibrate()
+                for l in range(args.nstochastic):
+                    chain.step(N=skip)
+                    grads[l] = Loss_grad(chain.x, g_params)  # gradient descent
+
+                grad = Grad_Mean(grads, weight)
+                updates, opt_state = opt_update_jit(grad, opt_state)
+                g_params = optax.apply_updates(g_params, updates)
+
+            '''
+            # tracking the size of gradient
+            grad_abs = 0.
+        
+            grad_abs += np.linalg.norm(grad['params']
+                                       ['Dense_'+str(0)]['kernel'])
+            grad_abs += np.linalg.norm(grad['params']
+                                           ['Dense_'+str(0)]['bias'])
+
+                for j in range(args.layers):
+                    grad_abs += np.linalg.norm(grad['params']
+                                               ['MLP_'+str(0)]['Dense_'+str(j)]['kernel'])
+                    grad_abs += np.linalg.norm(grad['params']
+                                               ['MLP_'+str(0)]['Dense_'+str(j)]['bias'])
+            '''
+
+            # measurement once in a while
+            for i in range(len(obs)):
+                chain.step(N=skip)
+                obs[i] = model.observe(chain.x)
+                cvs[i] = f(chain.x, g_params)
+
+            # print(f'{np.mean(phases).real} {np.abs(np.mean(phases))} {bootstrap(np.array(phases))} ({np.mean(np.abs(chain.x))} {np.real(np.mean(acts))} {np.mean(acts)} {grad_abs} {chain.acceptance_rate()})', flush=True)
+            print(f'{np.mean(obs).real} {bootstrap(np.array(obs - cvs))} ({np.mean(np.abs(chain.x))} {chain.acceptance_rate()})', flush=True)
+
+            save()
+
+    except KeyboardInterrupt:
+        print()
+        save()
--- models/scalar.py
+++ models/scalar.py
@@ -22,6 +22,7 @@ def sk_contour(nbeta, nt, shape='S'):
     else:
         raise "TODO"
 
+
 @dataclass
 class Model:
     geom: Tuple[int]
@@ -35,13 +36,17 @@ class Model:
     def __post_init__(self):
         self.D = len(self.geom)
         self.NT = self.nbeta + 2*self.nt
-        self.dof = np.prod(self.geom,dtype=int)*self.NT
+        self.dof = np.prod(self.geom, dtype=int)*self.NT
         self.shape = (self.NT,)+self.geom
         self.contour = sk_contour(self.nbeta, self.nt, shape=self.sk_shape)
         self.contour_site = jnp.array(
-                [(self.contour[i]+self.contour[(i-1)%self.NT])/2 for i in range(self.NT)])
-        self.dt_link = self.dt*jnp.tile(self.contour.reshape((self.NT,)+(1,)*self.D), (1,)+self.geom)
-        self.dt_site = self.dt*jnp.tile(self.contour_site.reshape((self.NT,)+(1,)*self.D), (1,)+self.geom)
+            [(self.contour[i]+self.contour[(i-1) % self.NT])/2 for i in range(self.NT)])
+        self.dt_link = self.dt * \
+            jnp.tile(self.contour.reshape(
+                (self.NT,)+(1,)*self.D), (1,)+self.geom)
+        self.dt_site = self.dt * \
+            jnp.tile(self.contour_site.reshape(
+                (self.NT,)+(1,)*self.D), (1,)+self.geom)
 
         # Backwards compatibility
         self.periodic_contour = False
@@ -51,7 +56,7 @@ class Model:
         phi = phi.reshape(self.shape)
         pot = jnp.sum((self.dt_site)*(m2/2*phi**2))
         kin_s = [jnp.sum((self.dt_site)*(jnp.roll(phi, -1, axis=d)-phi)**2)/2
-                for d in range(1,self.D+1)]
+                 for d in range(1, self.D+1)]
         kin_t = jnp.sum((jnp.roll(phi, -1, axis=0) - phi)**2/(2*self.dt_link))
         return pot + jnp.sum(jnp.array(kin_s)) + kin_t
 
@@ -67,16 +72,18 @@ class Model:
         phi = phi.reshape(self.shape)
         pot = jnp.sum((self.dt_site)*(t*m2/2*phi**2 + lamda*phi**4))
         kin_s = [jnp.sum((self.dt_site)*(jnp.roll(phi, -1, axis=d)-phi)**2)/2
-                for d in range(1,self.D+1)]
+                 for d in range(1, self.D+1)]
         kin_t = jnp.sum((jnp.roll(phi, -1, axis=0) - phi)**2/(2*self.dt_link))
         return pot + t*(jnp.sum(jnp.array(kin_s)) + kin_t)
 
     def _observe(self, phi):
-        #return jnp.array([phi[0]*phi[i] for i in range(self.NT)] + [self._action(phi)])
-        phi_re = phi.reshape(self.shape)  
-        #return jnp.array([jnp.mean(phi_re * jnp.roll(phi_re, -i, axis=1)) for i in range(int(self.dof/self.NT))] + [self._action(phi)]) # only for 1D
-        phi_av = jnp.mean(phi_re, axis=1) # only for 1D
-        return jnp.array([jnp.mean(phi_av * jnp.roll(phi_av, -i)) for i in range(self.NT)] + [self._action(phi)])
+        # return jnp.array([phi[0]*phi[i] for i in range(self.NT)] + [self._action(phi)])
+        # phi_re = phi.reshape(self.shape)
+        # return jnp.array([jnp.mean(phi_re * jnp.roll(phi_re, -i, axis=1)) for i in range(int(self.dof/self.NT))] + [self._action(phi)]) # only for 1D
+        # phi_av = jnp.mean(phi_re, axis=1) # only for 1D
+        # return jnp.array([jnp.mean(phi_av * jnp.roll(phi_av, -i)) for i in range(self.NT)] + [self._action(phi)])
+
+        return phi[0] * phi[self.dof//2]
 
     def _phi(self, z):
         if self.lamda == 0:
@@ -98,66 +105,3 @@ class Model:
 
     def observe(self, z):
         return self._observe(self._phi(z))
-
-@dataclass
-class Model3:
-    geom: Tuple[int]
-    nbeta: int
-    nt: int
-    m2: float
-    lamda: float
-    dt: float = 1.
-    sk_shape: str = 'S'
-
-    def __post_init__(self):
-        self.D = len(self.geom)
-        self.NT = self.nbeta + 2*self.nt
-        self.dof = np.prod(self.geom,dtype=int)*self.NT
-        self.shape = (self.NT,)+self.geom
-        self.contour = sk_contour(self.nbeta, self.nt, shape=self.sk_shape)
-        self.contour_site = jnp.array(
-                [(self.contour[i]+self.contour[(i-1)%self.NT])/2 for i in range(self.NT)])
-        self.dt_link = self.dt*jnp.tile(self.contour.reshape((self.NT,)+(1,)*self.D), (1,)+self.geom)
-        self.dt_site = self.dt*jnp.tile(self.contour_site.reshape((self.NT,)+(1,)*self.D), (1,)+self.geom)
-
-        # Backwards compatibility
-        self.periodic_contour = False
-
-    def _action_quadratic(self, phi):
-        m2 = self.m2
-        phi = phi.reshape(self.shape)
-        pot = jnp.sum((self.dt_site)*(m2/2*phi**2))
-        kin_s = [jnp.sum((self.dt_site)*(jnp.roll(phi, -1, axis=d)-phi)**2)/2
-                for d in range(1,self.D+1)]
-        kin_t = jnp.sum((jnp.roll(phi, -1, axis=0) - phi)**2/(2*self.dt_link))
-        return pot + jnp.sum(jnp.array(kin_s)) + kin_t
-
-    def _action_cubic(self, phi):
-        lamda = self.lamda
-        phi = phi.reshape(self.shape)
-        pot = jnp.sum((self.dt_site)*(lamda*phi**3))
-        return pot
-
-    def _action(self, phi, t=1.):
-        m2 = self.m2
-        lamda = self.lamda
-        phi = phi.reshape(self.shape)
-        pot = jnp.sum((self.dt_site)*(t*m2/2*phi**2 + lamda*phi**4))
-        kin_s = [jnp.sum((self.dt_site)*(jnp.roll(phi, -1, axis=d)-phi)**2)/2
-                for d in range(1,self.D+1)]
-        kin_t = jnp.sum((jnp.roll(phi, -1, axis=0) - phi)**2/(2*self.dt_link))
-        return pot + t*(jnp.sum(jnp.array(kin_s)) + kin_t)
-
-    def _observe(self, phi):
-        #return jnp.array([phi[0]*phi[i] for i in range(self.NT)] + [self._action(phi)])
-        phi_re = phi.reshape(self.shape)  
-        #return jnp.array([jnp.mean(phi_re * jnp.roll(phi_re, -i, axis=1)) for i in range(int(self.dof/self.NT))] + [self._action(phi)]) # only for 1D
-        phi_av = jnp.mean(phi_re, axis=1) # only for 1D
-        return jnp.array([jnp.mean(phi_av * jnp.roll(phi_av, -i)) for i in range(self.NT)] + [self._action(phi)])
-
-
-    def action(self, z, t=1.):
-        return self._action(z, t=t)
-
-    def observe(self, z):
-        return self._observe(z)
