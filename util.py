ffb2ff491037f6336edb4afd4585d91ff3265299
Hyunwoo Oh <hyunwooh@umd.edu>
2023-10-13 13:25:08 -0400

    add util.py

---
+++ util.py
@@ -0,0 +1,58 @@
+import numpy as np
+import jax
+import jax.numpy as jnp
+
+def jackknife(xs, ws=None, Bs=10):  # Bs: Block size
+    B = len(xs)//Bs  # number of blocks
+    if ws is None:  # for reweighting
+        ws = xs*0 + 1
+    # Block
+    '''
+    x, w = [], []
+    for i in range(Bs):
+        x.append(sum(xs[i*B:i*B+B]*ws[i*B:i*B+B])/sum(ws[i*B:i*B+B]))
+        w.append(sum(ws[i*B:i*B+B]))
+    x = np.array(x)
+    w = np.array(w)
+    '''
+    x = np.array(xs[:B*Bs])
+    w = np.array(ws[:B*Bs])
+
+    m = sum(x*w)/sum(w)
+
+    # partition
+    block = [[B, Bs], list(x.shape[1:])]
+    block_f = [i for sublist in block for i in sublist]
+    x = x.reshape(block_f)
+    w = w.reshape(block_f)
+
+    # jackknife
+    vals = [np.mean(np.delete(x, i, axis=0)*np.delete(w, i, axis=0)) /
+            np.mean(np.delete(w, i)) for i in range(B)]
+    vals = np.array(vals)
+
+    return m, np.std(vals.real) + 1j*np.std(vals.imag)
+
+
+def bootstrap(xs, ws=None, N=100, Bs=50):
+    if Bs > len(xs):
+        Bs = len(xs)
+    B = len(xs)//Bs
+    if ws is None:
+        ws = xs*0 + 1
+    # Block
+    x, w = [], []
+    for i in range(Bs):
+        x.append(sum(xs[i*B:i*B+B]*ws[i*B:i*B+B])/sum(ws[i*B:i*B+B]))
+        w.append(sum(ws[i*B:i*B+B]))
+    x = np.array(x)
+    w = np.array(w)
+    # Regular bootstrap
+    y = x * w
+    m = (sum(y) / sum(w))
+    ms = []
+    for n in range(N):
+        s = np.random.choice(range(len(x)), len(x))
+        ms.append((sum(y[s]) / sum(w[s])))
+    ms = np.array(ms)
+    return m, np.std(ms.real) + 1j*np.std(ms.imag)
