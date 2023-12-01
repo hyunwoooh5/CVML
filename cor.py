0194c5b12f47d6e831c80286d37949d43e3ab18b
Hyunwoo Oh <hyunwooh@umd.edu>
2023-12-01 10:28:53 -0500

    add optimized correlation function

--- cor.py
+++ cor.py
@@ -60,8 +60,9 @@ conf=jnp.array(conf)
 obs_full = jax.vmap(model.observe)(conf)
 
 conf_partial=jnp.concatenate([conf[:args.n_test], conf[-args.n_train:]])
-cv=jax.vmap(lambda x: f(x, g_params))(conf_partial[:args.n_test])
+cv=jax.vmap(lambda x: f(x, g_params))(conf_partial)
 obs = jax.vmap(model.observe)(conf_partial)
+
 
 print("nocv")
 jack=[0]*nt
@@ -80,7 +81,8 @@ for i in range(nt):
 for i in range(nt):
     print(f"{jnp.array(cov).reshape([nt,nt])[i]}")
 
-print("nocv large")
+
+print("\nnocv large")
 jack_full=[0]*nt
 cov_full=[0]*(nt**2)
 avs_full = jax.vmap(jnp.mean)(obs_full.T)
@@ -96,6 +98,7 @@ for i in range(nt):
 
 for i in range(nt):
     print(f"{jnp.array(cov_full).reshape([nt,nt])[i]}")
+
 
 print("\ncv")
 jack_cv = [0]*nt
@@ -118,10 +121,11 @@ print(f"covariance:")
 for i in range(nt):
     print(f"{jnp.array(cov_cv).reshape([nt,nt])[i]}")
 
+
 print("\ncv all")
 jack_cv_all = [0]*nt
 cov_cv_all = [0]* (nt**2)
-obs_cv_all = obs[:args.n_test] - jnp.concatenate([cv]*nt).reshape([nt, args.n_test]).T
+obs_cv_all = obs[:args.n_test] - jnp.concatenate([cv[:args.n_test]]*nt).reshape([nt, args.n_test]).T
 avs_cv_all = jax.vmap(jnp.mean)(obs_cv_all.T)
 
 for i in range(nt):
@@ -134,4 +138,96 @@ for i in range(nt):
 print(f"covariance:")
 for i in range(nt):
     print(f"{jnp.array(cov_cv_all).reshape([nt,nt])[i]}")
+
+
+print(f"\nfully optimized")
+jack_optimized = [0]*nt
+cov_optimized = [0]*(nt**2)
+obs_optimized = obs[:args.n_test]
+
+for i in range(nt):
+    _, err_no = jack[i]
+    _, err_cv = jack_cv_all[i]
+    if err_no > err_cv:
+        jack_optimized[i] = jack_cv_all[i]
+        for j in range(args.n_test):
+            obs_optimized = obs_optimized.at[(j,i)].add(-cv[j])
+    else:
+        jack_optimized[i] = jack[i]
+    
+avs_optimized = jax.vmap(jnp.mean)(obs_optimized)
+print(f"jackknife:\n{jack_optimized}")
+
+for i in range(nt):
+    for j in range(nt):
+        cov_optimized[i*nt+j] = (obs_optimized[:,i]-avs_optimized[i])@(obs_optimized[:,j]-avs_optimized[j])/(args.n_test-1)/(args.n_test)
+print(f"covariance:")
+for i in range(nt):
+    print(f"{jnp.array(cov_optimized).reshape([nt,nt])[i]}")
+
+
+print("\ncv full samples")
+jack_cv_f = [0]*nt
+cov_cv_f = [0] * (nt**2)
+obs_cv_f = obs
+
+for i in range(args.n_test+args.n_train):
+    obs_cv_f = obs_cv_f.at[(i,nt//2)].add(-cv[i])
+
+avs_cv_f = jax.vmap(jnp.mean)(obs_cv_f.T)
+
+for i in range(nt):
+    jack_cv_f[i] = jackknife(obs_cv_f[:,i])
+print(f"jackknife:\n{jack_cv_f}")
+
+for i in range(nt):
+    for j in range(nt):
+        cov_cv_f[i*nt+j] = (obs_cv_f[:,i]-avs_cv_f[i])@(obs_cv_f[:,j]-avs_cv_f[j])/(args.n_test+args.n_train-1)/(args.n_test+args.n_train)
+print(f"covariance:")
+for i in range(nt):
+    print(f"{jnp.array(cov_cv_f).reshape([nt,nt])[i]}")
+
+
+print("\ncv all full samples")
+jack_cv_all_f = [0]*nt
+cov_cv_all_f = [0]* (nt**2)
+obs_cv_all_f = obs - jnp.concatenate([cv]*nt).reshape([nt, args.n_test+args.n_train]).T
+avs_cv_all_f = jax.vmap(jnp.mean)(obs_cv_all_f.T)
+
+for i in range(nt):
+    jack_cv_all_f[i] = jackknife(obs_cv_all_f[:,i])
+print(f"jackknife:\n{jack_cv_all_f}")
+
+for i in range(nt):
+    for j in range(nt):
+        cov_cv_all_f[i*nt+j] = (obs_cv_all_f[:,i]-avs_cv_all_f[i])@(obs_cv_all_f[:,j]-avs_cv_all_f[j])/(args.n_test+args.n_train-1)/(args.n_test+args.n_train)
+print(f"covariance:")
+for i in range(nt):
+    print(f"{jnp.array(cov_cv_all_f).reshape([nt,nt])[i]}")
+
+
+print(f"\nfully optimized full samples")
+jack_optimized_f = [0]*nt
+cov_optimized_f = [0]*(nt**2)
+obs_optimized_f = obs
+
+for i in range(nt):
+    _, err_no = jack[i]
+    _, err_cv = jack_cv_all_f[i]
+    if err_no > err_cv:
+        jack_optimized_f[i] = jack_cv_all_f[i]
+        for j in range(args.n_test+args.n_train):
+            obs_optimized_f = obs_optimized_f.at[(j,i)].add(-cv[j])
+    else:
+        jack_optimized_f[i] = jack[i]
+    
+avs_optimized_f = jax.vmap(jnp.mean)(obs_optimized_f)
+print(f"jackknife:\n{jack_optimized_f}")
+
+for i in range(nt):
+    for j in range(nt):
+        cov_optimized_f[i*nt+j] = (obs_optimized_f[:,i]-avs_optimized_f[i])@(obs_optimized_f[:,j]-avs_optimized_f[j])/(args.n_test+args.n_train-1)/(args.n_test+args.n_train)
+print(f"covariance:")
+for i in range(nt):
+    print(f"{jnp.array(cov_optimized_f).reshape([nt,nt])[i]}")
 
