2d2f7ba03ee3f22951ab06ae1391d7584cbc3ac7
Hyunwoo Oh <hyunwooh@umd.edu>
2024-01-17 15:31:21 -0500

    add degree of freedom and number of configs for converter

--- converter.py
+++ converter.py
@@ -15,20 +15,30 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('geom', type=str, help='geometry')
+parser.add_argument(
+    'dof', type=int, help='degree of freedom for each configuration')
 parser.add_argument('input', type=str, help='text file')
 parser.add_argument('output', type=str, help='pickle file')
+parser.add_argument('-n', '--nconfig', type=int,
+                    default=-1, help='number of configs')
 
 args = parser.parse_args()
 
 GEOM = ast.literal_eval(args.geom)
-dof = np.prod(GEOM, dtype=int)
+n_lat = np.prod(GEOM, dtype=int)
 
 with open(args.input, 'rb') as f:
     conf = jnp.array([float(x) for x in f.readlines()])
 
-n_conf = int(len(conf)/dof)
+n = len(conf)//args.dof
+n_conf = n//n_lat
 
-conf = conf.reshape([n_conf, dof])
+conf = conf.reshape([n, args.dof])
+
+key = jax.random.PRNGKey(0)
+conf_shuffled = jax.random.permutation(key, conf)
+
+conf_shuffled = conf_shuffled.reshape([n_conf, args.dof*n_lat])
 
 with open(args.output, 'wb') as f:
-    pickle.dump(conf, f)
+    pickle.dump(conf_shuffled[:args.nconfig], f)
