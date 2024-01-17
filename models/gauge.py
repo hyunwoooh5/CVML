3e330f582e9f4cc0499e5d90e51e0af87362a12d
Hyunwoo Oh <hyunwooh@umd.edu>
2024-01-17 14:30:24 -0500

    add SU(2) gauge theory

--- models/gauge.py
+++ models/gauge.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Tuple
+from functools import reduce
 
 import jax.numpy as jnp
 import numpy as np
@@ -21,9 +22,31 @@ class U1_2D_OBC:
 
     def observe(self, phi):
         phi = phi.reshape(self.shape)
-        # return jnp.prod(jnp.exp(1j*phi[:self.shape[0], :self.shape[1]]))
-        obs = jnp.mean(jnp.array([jnp.prod(jnp.exp(1j*(jnp.roll(phi, (i, j), axis=(0, 1))[:self.shape[0], :self.shape[1]])))
-                                  for i in range(self.shape[0]) for j in range(self.shape[1])]))  # Move the area and take average, full area
-        #obs = jnp.array([jnp.mean(jnp.array([jnp.prod(jnp.exp(1j*(jnp.roll(phi, (i, j), axis=(0, 1))[:k, :k])))
+        return jnp.prod(jnp.exp(1j*phi[:self.shape[0], :self.shape[1]]))
+        # Move the area and take average, full area
+        # obs = jnp.array([jnp.mean(jnp.array([jnp.prod(jnp.exp(1j*(jnp.roll(phi, (i, j), axis=(0, 1))[:k, :k])))
         #                                     for i in range(self.shape[0]) for j in range(self.shape[1])])) for k in range(self.shape[0])])
         return obs
+
+
+@dataclass
+class SU2_2D_OBC:
+    geom: Tuple[int]
+    g: float
+
+    def __post_init__(self):
+        self.dof = np.prod(self.geom, dtype=int)
+        self.shape = self.geom
+
+        self.periodic = True
+
+    def action(self, phi):
+        phi = phi.reshape([self.dof, 2**2-1])
+
+        return jnp.sum(-4./(self.g**2)*jnp.sin(phi[:, 0])*jnp.cos(phi[:, 1])-jnp.log(jnp.sin(2*phi[:, 0])))
+
+    def observe(self, phi):
+        phi = phi.reshape([self.dof, 2**2-1])
+        plaq = jnp.array([[jnp.sin(phi[:, 0])*jnp.exp(1j*phi[:, 1]),
+                         jnp.cos(phi[:, 0])*jnp.exp(1j*phi[:, 2])], [-jnp.cos(phi[:, 0])*jnp.exp(-1j*phi[:, 2]), jnp.sin(phi[:, 0])*jnp.exp(-1j*phi[:, 1])]]).transpose(2, 0, 1)
+        return jnp.trace(reduce(jnp.matmul, plaq))
---
+++ sample_su2_2d_obc.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <random>
+#include <Eigen/Dense> // #include </home/oh/eigen-3.3.9/Eigen/Dense>
+#include <fstream>
+#include <ctime> // Timer
+#include <stdio.h>
+
+typedef std::complex<double> dcomp;
+const dcomp I(0, 1);
+const double PI = std::atan(1.0) * 4;
+
+// Random Number Generator
+// std::default_random_engine generator; // for random engine reset
+std::random_device generator;                           // get non-deterministic(truly random) seed
+std::mt19937 gen(generator());                          // reset RNG
+std::uniform_real_distribution<double> dist(-1.0, 1.0); // -1.0 to 1.0 uniform distribution
+std::uniform_real_distribution<> rand01(0.0, 1.0);      // For Metropolis
+
+int accept = 0; // For acceptance rate, Should not be defined again
+
+// Functions
+struct params
+{
+    int dof;
+    double g, delta;
+    int n_thermal, n_conf;
+};
+
+dcomp Log_Det(const Eigen::MatrixXcd &m, Eigen::MatrixXcd *inv = NULL)
+{
+    Eigen::PartialPivLU<Eigen::MatrixXcd> lu(m); // LU decomposition of M
+    dcomp res = 0;
+    for (int i = 0; i < m.col(0).size(); ++i)
+        res += log(lu.matrixLU()(i, i)); // Calculating LogDet
+
+    res += (lu.permutationP().determinant() == -1) ? I * PI : 0.0;
+    res -= I * 2.0 * PI * round(res.imag() / (2.0 * PI));
+    if (inv != NULL)
+        *inv = lu.inverse();
+    return res;
+}
+
+// Metropolis
+double Action(Eigen::ArrayXd &A, params &p)
+{
+    if (A[0] < 0 || A[0] > PI / 2)
+    {
+        return pow(10, 10);
+    }
+    else
+    {
+        return -4. / pow(p.g, 2) * sin(A[0]) * cos(A[1]) - log(sin(2 * A[0]));
+    }
+}
+
+Eigen::ArrayXd Metropolis(Eigen::ArrayXd &A, params &p)
+{
+    Eigen::ArrayXd A_new = A + p.delta * Eigen::ArrayXd::NullaryExpr(p.dof, [&]()
+                                                                     { return dist(gen); });
+
+    double dS = Action(A_new, p) - Action(A, p);
+
+    if (exp(-dS) >= rand01(gen))
+    {
+        accept++;
+
+        return A_new;
+    }
+    else
+    {
+        return A;
+    }
+}
+
+Eigen::ArrayXd Sweep(Eigen::ArrayXd &A, params &p)
+{
+    for (int i = 0; i < p.n_conf; i++)
+    {
+        for (int j = 0; j < p.dof * 100; j++)
+        {
+            A = Metropolis(A, p);
+        }
+        std::cout << A << std::endl;
+    }
+
+    return A;
+}
+
+Eigen::ArrayXd Thermalization(Eigen::ArrayXd &A, params &p)
+{
+    for (int i = 0; i < p.dof * p.n_thermal; i++)
+    {
+        A = Metropolis(A, p);
+    }
+
+    return A;
+}
+
+Eigen::ArrayXd Calibrate(Eigen::ArrayXd &A, params &p)
+{
+    double ratio = 0;
+    while (ratio <= 0.3 || ratio >= 0.55)
+    {
+        accept = 0;
+        for (int i = 0; i < 10 * p.dof; i++)
+        {
+            A = Metropolis(A, p);
+        }
+        ratio = (double)accept / (p.dof * 10);
+        if (ratio >= 0.55)
+        {
+            p.delta = p.delta * 1.02;
+        }
+        else if (ratio <= 0.3)
+        {
+            p.delta = p.delta * 0.98;
+        }
+    }
+
+    return A;
+}
+
+int main(int argc, char **argv)
+{
+    struct params p;
+    p.delta = 1;
+    p.dof = pow(2, 2) - 1;
+
+    p.g = std::stod(argv[1]);
+    p.n_thermal = 10000;
+    p.n_conf = std::stoi(argv[2]);
+
+    Eigen::ArrayXd configuration = Eigen::ArrayXd::Zero(p.dof); // Cold start
+
+    Calibrate(configuration, p);
+    Thermalization(configuration, p);
+    Calibrate(configuration, p);
+    Sweep(configuration, p);
+
+    return 0;
+}
