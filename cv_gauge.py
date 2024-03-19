23624665453f7b44443bf7d22b74cf1fdf4a8558
Hyunwoo Oh <hyunwooh@umd.edu>
2024-03-19 19:36:26 -0400

    add basics of control variates for gauge theory

---
+++ cv_gauge.py
@@ -0,0 +1,331 @@
+#!/usr/bin/env python
+
+from models import gauge
+import pickle
+import sys
+import time
+from typing import Callable, Sequence
+
+import flax
+import flax.linen as nn
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+from util import *
+from itertools import product
+
+jax.config.update("jax_debug_nans", True)
+jax.config.update("jax_debug_infs", True)
+
+
+@jax.jit
+def arcsinh(x: any) -> any:
+    return jnp.arcsinh(x)
+
+
+@jax.jit
+def sinh(x: any) -> any:
+    return jnp.sinh(x)
+
+
+class MLP(nn.Module):
+    volume: int
+    features: Sequence[int]
+    kernel_init: Callable = nn.initializers.variance_scaling(
+        2, "fan_in", "truncated_normal")  # for ReLU / CELU
+    bias_init: Callable = nn.initializers.zeros
+
+    @nn.compact
+    def __call__(self, x):
+        for feat in self.features:
+            x = nn.Dense(feat, use_bias=False,
+                         kernel_init=self.kernel_init,
+                         bias_init=self.bias_init)(x)
+            x = arcsinh(x)
+        x = nn.Dense(1, use_bias=False,
+                     kernel_init=self.kernel_init)(x)
+        return x
+
+
+class CV_MLP(nn.Module):
+    volume: int
+    features: Sequence[int]
+
+    @nn.compact
+    def __call__(self, x):
+        x = MLP(self.volume, self.features)(x)
+        y = self.param('bias', nn.initializers.zeros, (1,))
+        return x, y
+
+
+class CV_MLP_Periodic(nn.Module):
+    volume: int
+    features: Sequence[int]
+
+    @nn.compact
+    def __call__(self, x):
+        input = jnp.ravel(jnp.array([jnp.sin(x), jnp.cos(x)]))
+
+        x = MLP(self.volume, self.features)(input)
+        y = self.param('bias', nn.initializers.zeros, (1,))
+        return x, y
+
+
+class CNN(nn.Module):
+    volume: int
+    features: Sequence[int]
+    kernel_init: Callable = nn.initializers.variance_scaling(
+        2, "fan_in", "truncated_normal")  # for ReLU / CELU
+    bias_init: Callable = nn.initializers.zeros
+
+    @nn.compact
+    def __call__(self, x):
+        for feat in self.features:
+            x = nn.Conv(feat, kernel_size=(3, 3), use_bias=True, kernel_init=self.kernel_init,
+                        bias_init=self.bias_init, padding='CIRCULAR')(x)  # Periodic boundary
+            # x = arcsinh(x)
+            x = nn.leaky_relu(x, negative_slope=0.01)
+
+        x = nn.Conv(1, kernel_size=(3, 3), use_bias=True, kernel_init=self.kernel_init,
+                    bias_init=self.bias_init, padding='CIRCULAR')(x)
+        x = nn.tanh(x)
+
+        y = self.param('bias', nn.initializers.zeros, (1,))
+        return jnp.ravel(x), y
+
+
+class CV_CNN(nn.Module):
+    volume: int
+    features: Sequence[int]
+
+    def __post_init__(self):
+        self.mask_odd = jnp.arange(self.volume) % 2+0.
+        self.mask_even = 1. - self.mask_odd
+        super().__post_init__()
+
+    @nn.compact
+    def __call__(self, x, shape):
+        x = jnp.exp(1j*x)
+        x = x.reshape(shape)
+        # x_odd = self.mask_odd*x
+        # x_even = self.mask_even*x
+        # x_odd = x_odd.reshape(shape)
+        # x_even = x_even.reshape(shape)
+        # x = jnp.array([x_odd, x_even]).T
+        x = CNN(self.volume, self.features)(x)
+        return x
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="Train g",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        fromfile_prefix_chars='@')
+    parser.add_argument('model', type=str, help="model filename")
+    parser.add_argument('cv', type=str, help="cv filename")
+    parser.add_argument('cf', type=str, help="configurations file name")
+    parser.add_argument('-i', '--init', action='store_true',
+                        help="re-initialize even if cv already exists")
+    parser.add_argument('-f', '--from', dest='fromfile', type=str,
+                        help="initialize from other file")
+    parser.add_argument('-l', '--layers', type=int, default=0,
+                        help='number of (hidden) layers')
+    parser.add_argument('-w', '--width', type=int, default=1,
+                        help='width')
+    parser.add_argument('--seed', type=int, default=0, help="random seed")
+    parser.add_argument('--seed-time', action='store_true',
+                        help="seed PRNG with current time")
+    parser.add_argument('--dp', action='store_true',
+                        help="turn on double precision")
+    parser.add_argument('-lr', '--learningrate', type=float, default=1e-4,
+                        help="learning rate")
+    parser.add_argument('-N', '--nstochastic', default=1, type=int,
+                        help="number of samples to estimate gradient")
+    parser.add_argument('-o',  '--optimizer', choices=['adam', 'sgd', 'yogi'], default='adam',
+                        help='optimizer to use')
+    parser.add_argument('-s', '--schedule', action='store_true',
+                        help="scheduled learning rate")
+    parser.add_argument('-C', '--care', type=float, default=1,
+                        help='scaling for learning schedule')
+    parser.add_argument('--b1', type=float, default=0.9,
+                        help="b1 parameter for adam")
+    parser.add_argument('--b2', type=float, default=0.999,
+                        help="b2 parameter for adam")
+    parser.add_argument('-nt', '--n_train', type=int, default=1000,
+                        help="number of training set")
+    parser.add_argument('-ns', '--n_test', type=int, default=1000,
+                        help="number of test set")
+    parser.add_argument('--l2', type=float, default=0.0,
+                        help="l2 regularization")
+    parser.add_argument('-opt', '--optuna', action='store_true',
+                        help="Use optuna")
+    parser.add_argument('--cnn',  action='store_true',
+                        help="Use CNN")
+
+    args = parser.parse_args()
+
+    if args.dp:
+        jax.config.update('jax_enable_x64', True)
+
+    seed = args.seed
+    if args.seed_time:
+        seed = time.time_ns()
+    key = jax.random.PRNGKey(seed)
+
+    with open(args.model, 'rb') as f:
+        model = eval(f.read())
+    V = model.dof
+
+    g_ikey, chain_key = jax.random.split(key, 2)
+
+    # define control variates
+    loaded = False
+    if not args.init and not args.fromfile:
+        try:
+            with open(args.cv, 'rb') as f:
+                g, g_params = pickle.load(f)
+            loaded = True
+        except FileNotFoundError:
+            pass
+    if args.fromfile:
+        with open(args.fromfile, 'rb') as f:
+            g, g_params = pickle.load(f)
+        loaded = True
+    if not loaded:
+        if args.cnn:
+            g = CV_CNN(V, [args.width]*args.layers)
+            g_params = g.init(g_ikey, jnp.zeros(V), model.shape)
+
+            dS = jax.jit(jax.grad(lambda y: model.action(y).real))
+            j = jax.jit(jax.jacfwd(lambda x, p: g.apply(
+                p, x, model.shape)[0], argnums=0))
+
+            '''
+            print(dS(jax.random.normal(g_ikey, (V,))))
+            print(g.apply(g_params, jax.random.normal(
+                g_ikey, model.shape), model.shape)[0])
+            print(jnp.ravel(dS(jax.random.normal(g_ikey, model.shape)))@jnp.ravel(
+                g.apply(g_params, jax.random.normal(g_ikey, model.shape), model.shape)[0]))
+            print(g.apply(g_params, jax.random.normal(
+                g_ikey, model.shape), model.shape))
+            print(j(jax.random.normal(g_ikey, (V,)), g_params).shape)
+            print(jnp.trace(j(jax.random.normal(g_ikey, (V,)), g_params)))
+            '''
+
+            @jax.jit
+            def f(x, p):
+                dg = jnp.trace(j(x, p))
+                ds = dS(x)
+                gx, _ = g.apply(p, x, model.shape)
+
+                return dg - gx@ds
+
+            # define loss function
+
+            @jax.jit
+            def Loss(x, p):
+                _, y = g.apply(p, x, model.shape)
+
+                # shift is not regularized
+                return jnp.abs(model.observe(x, 8) - f(x, p) - y[0])**2 + sum(l2_loss(w, alpha=args.l2) for w in jax.tree_util.tree_leaves(p["params"])) - args.l2 * y[0]**2
+
+            '''
+            print(f(jax.random.normal(g_ikey, (V,)), g_params))
+            print(Loss(jax.random.normal(g_ikey, (V,)), g_params))
+            '''
+
+            def save():
+                with open(args.cv, 'wb') as aa:
+                    pickle.dump((g, g_params), aa)
+
+        else:
+            if model.periodic:
+                g1 = CV_MLP_Periodic(V, [args.width]*args.layers)
+                g_params = g1.init(g_ikey, jnp.zeros(V))
+            else:
+                g1 = CV_MLP(V, [args.width]*args.layers)
+                g_params = g1.init(g_ikey, jnp.zeros(V))
+
+            # g(Tx) = Tg(x)
+            index = jnp.array(
+                [(-i, -j) for i, j in product(*list(map(lambda y: range(y), model.shape)))])
+
+            @jax.jit
+            def g(x, p):
+                def g_(x, p, ind):
+                    return g1.apply(p, jnp.roll(x.reshape(model.shape), ind, axis=(0, 1)).reshape(V))[0]
+                return jnp.ravel(jax.vmap(lambda ind: g_(x, p, ind))(index).T)
+
+            g1_grad = jax.jit(
+                jax.grad(lambda y, p: g1.apply(p, y)[0][0], argnums=0))
+            dS = jax.jit(jax.grad(lambda y: model.action(y).real))
+            jaco = jax.jit(jax.jacfwd(g, argnums=0))
+
+            # define subtraction function
+            @jax.jit
+            def f(x, p):
+                # diagonal sum (Stein's identity)
+                def diag_(ind):
+                    return g1_grad(jnp.roll(x.reshape(model.shape), ind, axis=(0, 1)).reshape(V), p)
+                j = jax.vmap(diag_)(index)[:, 0].sum()
+                return j - g(x, p)@dS(x)
+
+            # define loss function
+            @jax.jit
+            def Loss(x, p):
+                _, y = g1.apply(p, x)
+
+                # shift is not regularized
+                return jnp.abs(model.observe(x) - f(x, p) - y[0])**2 + sum(l2_loss(w, alpha=args.l2) for w in jax.tree_util.tree_leaves(p["params"])) - args.l2 * y[0]**2
+
+            def save():
+                with open(args.cv, 'wb') as aa:
+                    pickle.dump((g1, g_params), aa)
+
+    Loss_grad = jax.jit(jax.grad(lambda x, p: Loss(x, p), argnums=1))
+
+    if args.schedule:
+        sched = optax.exponential_decay(
+            init_value=args.learningrate,
+            transition_steps=int(args.care),
+            decay_rate=0.1,
+            end_value=1e-5)
+    else:
+        sched = optax.constant_schedule(args.learningrate)
+    opt = getattr(optax, args.optimizer)(sched, args.b1, args.b2)
+    opt_state = opt.init(g_params)
+    opt_update_jit = jax.jit(opt.update)
+
+    # measurement
+    with open(args.cf, 'rb') as aa:  # variable name aa should be different from f
+        configs = pickle.load(aa)
+
+    # separate configurations for training and test
+    configs = jnp.array(configs)
+    # configs = jnp.log(configs).imag
+    configs_test = configs[:args.n_test]
+    configs = configs[-args.n_train:]
+    obs = jax.vmap(lambda y: model.observe(y, 8))(configs_test)
+    obs_av = jackknife(np.array(obs))
+
+    # Training
+    while True:
+        g_ikey, subkey = jax.random.split(g_ikey)
+        configs = jax.random.permutation(subkey, configs)
+        for s in range(args.n_train//args.nstochastic):  # one epoch
+            grads = jax.vmap(lambda y: Loss_grad(y, g_params))(
+                configs[args.nstochastic*s: args.nstochastic*(s+1)])
+
+            grad = jax.tree_util.tree_map(
+                lambda x: jnp.mean(x, axis=0), grads)
+            updates, opt_state = opt_update_jit(grad, opt_state)
+            g_params = optax.apply_updates(g_params, updates)
+
+        fs = jax.vmap(lambda x: f(x, g_params))(configs_test)
+        ls = jnp.mean(jax.vmap(lambda x: Loss(x, g_params))(configs_test))
+
+        print(
+            f"{obs_av} {jackknife(np.array(obs-fs))} {jackknife(np.array(fs))} {ls}", flush=True)
+        save()
--- models/gauge.py
+++ models/gauge.py
@@ -2,8 +2,30 @@ from dataclasses import dataclass
 from typing import Tuple
 from functools import reduce
 
+import jax
 import jax.numpy as jnp
 import numpy as np
+
+
+@dataclass
+class Lattice:
+    shape: Tuple
+
+    def __post_init__(self):
+        self.dof = np.prod(self.shape, dtype=int)
+        self.V = self.dof//self.shape[2]
+
+    def idx(self, *args):
+        n = len(args)
+        return jnp.ravel_multi_index(args, self.shape[-n:], mode='wrap')
+
+    def plaquettes(self):
+        index = []
+        for x in range(self.shape[0]):
+            for y in range(self.shape[1]):
+                index.append([[x, y, 0], [(x+1) % self.shape[0], y, 1],
+                             [x, (y+1) % self.shape[1], 0], [x, y, 1]])
+        return jnp.array(index)
 
 
 @dataclass
@@ -15,18 +37,123 @@ class U1_2D_OBC:
         self.dof = np.prod(self.geom, dtype=int)
         self.shape = self.geom
 
-        self.periodic = True
-
     def action(self, phi):
         return -self.beta*jnp.cos(phi).sum()
 
     def observe(self, phi):
         phi = phi.reshape(self.shape)
-        return jnp.prod(jnp.exp(1j*phi[:self.shape[0], :self.shape[1]]))
+        # return jnp.array([jnp.prod(jnp.exp(1j*phi[:k, :k])) for k in range(1, self.shape[0]+1)])
+        # return jnp.array([jnp.mean(jnp.array([jnp.prod(jnp.exp(1j*(jnp.roll(phi, (i, j), axis=(0, 1))[:k, :k])))
+        #                                     for i in range(self.shape[0]) for j in range(self.shape[1])])) for k in range(1, self.shape[0]+1)])
+        return jnp.prod(jnp.exp(1j*phi))
         # Move the area and take average, full area
         # obs = jnp.array([jnp.mean(jnp.array([jnp.prod(jnp.exp(1j*(jnp.roll(phi, (i, j), axis=(0, 1))[:k, :k])))
         #                                     for i in range(self.shape[0]) for j in range(self.shape[1])])) for k in range(self.shape[0])])
         return obs
+
+
+@dataclass
+class U1_2D_PBC:
+    geom: Tuple[int]
+    beta: float
+
+    def __post_init__(self):
+        self.shape = (self.geom[0], self.geom[1], 2)
+
+        self.lattice = Lattice(self.shape)
+        self.dof = self.lattice.dof
+        self.V = self.lattice.V
+
+        self.plaq = self.lattice.plaquettes()
+
+    def plaquette(self, phi):
+        # phi = jnp.exp(1j*phi)
+        phi = phi.reshape(self.shape1)
+        return jax.vmap(lambda y: y[0]*y[1]/y[2]/y[3])(jax.vmap(jax.vmap(lambda y: phi[*y]))(self.plaq))
+        x = jnp.array([j[0]*j[1]/j[2]/j[3] for j in [jax.vmap(lambda y: phi[*y])(self.plaq[i])
+                      for i in range(self.V)]])
+        return x
+
+        x = jnp.array([jax.vmap(lambda y: phi[*y])(self.plaq[i])
+                      for i in range(self.V)])
+        y = jnp.array([x[i][0]*x[i][1]/x[i][2]/x[i][3] for i in range(self.V)])
+        return y
+
+    def action(self, phi):
+        return self.beta*jnp.sum(1-jnp.cos(1*phi))
+
+    def observe(self, phi, i):
+        x = jnp.exp(1j*phi)
+        return jnp.prod(x[:i])
+
+    def action1(self, phi):
+        return self.beta*jnp.sum(1-self.plaquette(phi)).real
+        for i in range(self.V):
+            u1, u2, u3, u4 = jax.vmap(lambda y: phi[*y])(self.plaq[i])
+            s += 2.*(u1*u2/u3/u4).real
+        return -self.beta*s
+
+    def observe1(self, phi, i):
+        x = self.plaquette(phi)
+        return jnp.prod(x[:i])
+        plaq = self.lattice.plaquettes()
+
+        return jnp.prod(plaq[:i])
+
+        phi = phi.reshape(self.shape)
+        return jnp.prod(jnp.exp(1j*phi))
+
+    def plaquette3(self, phi):
+        phi = phi.reshape(self.shape)
+        return jax.vmap(lambda y: y[0]*y[1]/y[2]/y[3])(jax.vmap(jax.vmap(lambda y: phi[*y]))(self.plaq))
+        x = jnp.array([j[0]*j[1]/j[2]/j[3] for j in [jax.vmap(lambda y: phi[*y])(self.plaq[i])
+                      for i in range(self.V)]])
+        return x
+
+        x = jnp.array([jax.vmap(lambda y: phi[*y])(self.plaq[i])
+                      for i in range(self.V)])
+        y = jnp.array([x[i][0]*x[i][1]/x[i][2]/x[i][3] for i in range(self.V)])
+        return y
+
+    def action3(self, phi):
+        return -self.beta*jnp.sum(self.plaquette(phi)).real
+        for i in range(self.V):
+            u1, u2, u3, u4 = jax.vmap(lambda y: phi[*y])(self.plaq[i])
+            s += 2.*(u1*u2/u3/u4).real
+        return -self.beta*s
+
+    def observe3(self, phi, i):
+        x = self.plaquette(phi)
+        return jnp.prod(x[:i])
+        plaq = self.lattice.plaquettes()
+
+        return jnp.prod(plaq[:i])
+
+        phi = phi.reshape(self.shape)
+        return jnp.prod(jnp.exp(1j*phi))
+
+    def action2(self, phi):
+        s = 0
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                s += 2.*(phi[i, j, 0] * phi[(i+1) % self.shape[0], j, 1] /
+                         phi[i, (j+1) % self.shape[1], 0]/phi[i, j, 1]).real
+        return -self.beta*s
+
+    def observe2(self, phi, i):
+        s = 1
+        for j in range(i):
+            x = j // self.shape[0]
+            y = j % self.shape[0]
+            s *= phi[x, y, 0] * phi[(x+1) % self.shape[0], y, 1] / \
+                phi[x, (y+1) % self.shape[1], 0]/phi[x, y, 1]
+        return s
+        plaq = self.lattice.plaquettes()
+
+        return jnp.prod(plaq[:i])
+
+        phi = phi.reshape(self.shape)
+        return jnp.prod(jnp.exp(1j*phi))
 
 
 @dataclass
