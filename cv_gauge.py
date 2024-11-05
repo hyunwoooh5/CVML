2bc916f010ef8f9801a58c81ed86c666888faaff
Hyunwoo Oh <hyunwooh@umd.edu>
2024-11-05 09:06:12 -0500

    change jacfwd to jacrev

--- cv_gauge.py
+++ cv_gauge.py
@@ -211,8 +211,8 @@ if __name__ == '__main__':
 
     if type(g1) == CV_CNN:
         dS = jax.grad(lambda y: model.action(y).real)
-        j = jax.jacfwd(lambda x, p: g1.apply(
-            p, x, model.shape)[0], argnums=0)
+        j = jax.jacrev(lambda x, p: g1.apply(
+            p, x, model.shape)[0], argnums=0) # jacrev is more efficient than jacfwd for n_input > n_output
 
         @jax.jit
         def f(x, p):
@@ -241,7 +241,7 @@ if __name__ == '__main__':
 
         g1_grad = jax.grad(lambda y, p: g1.apply(p, y)[0][0], argnums=0)
         dS = jax.grad(lambda y: model.action(y).real)
-        jaco = jax.jacfwd(g, argnums=0)
+        jaco = jax.jacrev(g, argnums=0) # jacrev is more efficient than jacfwd for n_input > n_output
 
         # define subtraction function
         @jax.jit
