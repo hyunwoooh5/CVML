56069afb084cc02219525bf739fad35b5cbaf28d
Hyunwoo Oh <conch.lulls-0e@icloud.com>
2025-04-02 15:23:16 -0400

    fix converter by shuffling as an option

--- converter.py
+++ converter.py
@@ -15,30 +15,29 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('geom', type=str, help='geometry')
-parser.add_argument(
-    'dof', type=int, help='degree of freedom for each configuration')
 parser.add_argument('input', type=str, help='text file')
 parser.add_argument('output', type=str, help='pickle file')
-parser.add_argument('-n', '--nconfig', type=int,
+parser.add_argument('-n', '--n_config', type=int,
                     default=-1, help='number of configs')
+parser.add_argument('-s', '--shuffle', type=bool,
+                    default=False, help="shuffle the Monte Carlo chain fully (for U(1) OBC)")
 
 args = parser.parse_args()
 
 GEOM = ast.literal_eval(args.geom)
-n_lat = np.prod(GEOM, dtype=int)
+dof = np.prod(GEOM, dtype=int)
 
 with open(args.input, 'rb') as f:
     conf = jnp.array([float(x) for x in f.readlines()])
 
-n = len(conf)//args.dof
-n_conf = n//n_lat
+n_conf = int(len(conf)/dof)
 
-conf = conf.reshape([n, args.dof])
+conf = conf.reshape([n_conf, dof])
 
-key = jax.random.PRNGKey(0)
-conf_shuffled = jax.random.permutation(key, conf)
-
-conf_shuffled = conf_shuffled.reshape([n_conf, args.dof*n_lat])
+if args.shuffle:
+    key = jax.random.PRNGKey(0)
+    conf = jax.random.permutation(key, conf.ravel())
+    conf = conf.reshape([n_conf, dof])
 
 with open(args.output, 'wb') as f:
-    pickle.dump(conf_shuffled[:args.nconfig], f)
+    pickle.dump(conf[:args.n_config], f)
